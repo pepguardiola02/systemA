9cae2fe62251bb8af4841d02980ac972f3774e52
Amar <amar.karic@skola.botkyrka.se>
2020-09-03 11:16:19 +0200

    calculation

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
 #include <Arduino.h>
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("starting...");
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int value = analogRead(A0);
+  float voltage = 5.0 * value / 1024;
+  Serial.println(voltage);
+  delay(2000);
 }
